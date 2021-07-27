c7399e71a2710e64756bfe74b6aa132dd56acd8a
Athul Muralidhar <athul.m68@gmail.com>
2021-07-27 20:38:01 +0200

    made it work!!!

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@
 extern crate actix_web;
 
 use std::{env, io};
-use actix_web::{middleware, App, HttpServer};
+use actix_web::{middleware, App, HttpServer,rt};
 
+mod tweet;
 
-
-#[actix_rt::main]
+#[rt::main]
+// Ok!(#[actix_rt::main])
 async fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
@@ -14,6 +15,13 @@ async fn main() -> io::Result<()> {
     HttpServer::new(||{
         App::new()
             .wrap(middleware::Logger::default())
-            .service()
-    }).bind("0.0.0.0:8000")?.run().await;
+            .service(tweet::list)
+            .service(tweet::create)
+            .service(tweet::delete)
+    })
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await?;
+
+    Ok(())
 }
--- src/tweet.rs
+++ src/tweet.rs
@@ -1,4 +1,64 @@
-use actix_web::web::{Json, Path}
-use actix_web::HttpResponse
-use chrono::{DateTime, Utc}
-use serde::{Desertialize, Serialize}
+use actix_web::web::{Json, Path};
+use actix_web::HttpResponse;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Response<T> {
+    pub results: Vec<T>
+}
+
+pub type Tweets = Response<Tweet>;
+
+pub const APPLICATION_JSON: &str = "application/json";
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Tweet {
+    id: String,
+    created_at: DateTime<Utc>,
+    message: String,
+}
+
+impl Tweet {
+    pub fn new(message: String) -> Self{
+        Self{
+            id: Uuid::new_v4().to_string(),
+            created_at: Utc::now(),
+            message
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TweetRequest {
+    message: Option<String>
+}
+
+impl TweetRequest {
+    pub fn to_tweet(&self) -> Option<Tweet> {
+        match &self.message{
+            Some(message) => Some(Tweet::new(message.to_string())),
+            None => None
+        }
+    }
+}
+
+
+#[get("/tweets")]
+pub async fn list() -> HttpResponse {
+    let tweets = Tweets { results: vec![]};
+
+    HttpResponse::Ok().content_type(APPLICATION_JSON).json(tweets)
+}
+
+#[post("/tweets")]
+pub async fn create(tweet: Json<TweetRequest>) -> HttpResponse {
+    HttpResponse::Created().content_type(APPLICATION_JSON).json(tweet.to_tweet())
+}
+
+#[delete("/tweets/{id}")]
+pub async fn delete(_path: Path<String>) -> HttpResponse {
+    HttpResponse::NoContent().content_type(APPLICATION_JSON).await.unwrap()
+}
